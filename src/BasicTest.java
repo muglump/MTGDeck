a645bc327e6cad914099c85ed03cc89295d7fa6b
muglump <korahel@gmail.com>
2013-04-15 18:05:38 -0400

    Test case for the Basic rule set.

---
+++ src/BasicTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
+public class BasicTest {
+
+	@Test
+	public void testBasicNotNull() {
+		Basic test = new Basic();
+		Assert.assertNotNull(test);
+	}
+	
+	@Test public void testBasicHasCorrectDeckLimit(){
+		Basic test = new Basic();
+		Assert.assertEquals(60, test.DeckSize);
+	}
+
+}
