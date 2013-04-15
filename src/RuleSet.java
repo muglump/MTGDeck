85bd274d00c6bd7059cae8ba813868129a5c36d2
muglump <korahel@gmail.com>
2013-04-15 18:16:53 -0400

    Rule sets should work need to integrate with decks.

---
+++ src/RuleSet.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+
+public abstract class RuleSet {
+	public int DeckSize;
+	public ArrayList<String> sets;
+	public abstract boolean DeckValid(Deck deck);
+	public abstract boolean canBeAdded(MTGCard card, Deck deck);
+}
