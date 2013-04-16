07940b38908fd215857bf965c3ba079918b5f988
muglump <korahel@gmail.com>
2013-04-15 20:04:14 -0400

    Decks work now, so need to get it working with the user input, and language files for Friday.

---
+++ src/Deck.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+
+
+
+
+class Deck {
+	private enum Rules{
+		BASIC
+	}
+	public ArrayList<MTGCard> cards;
+	public RuleSet rules;
+	public Deck(){
+		this.cards = new ArrayList<MTGCard>();
+		this.rules = new Basic();
+	}
+	public Deck(String rule){
+		this.cards = new ArrayList<MTGCard>();
+		this.rules = determineRules(rule);
+	}
+	private RuleSet determineRules(String rule) {
+		String rules = rule.toUpperCase();
+		RuleSet ruleSet = null;
+		Rules r;
+		try{
+			r = Rules.valueOf(rules);
+		}
+		catch(IllegalArgumentException e){
+			System.out.println("Invalid Rule Default to Basic "+ rules);
+			r = Rules.BASIC;
+		}
+		
+		switch(r){
+		case BASIC:
+			ruleSet = new Basic();
+		}
+		return ruleSet;
+	}
+	
+	public boolean addCardToDeck(MTGCard card){
+		boolean valid = rules.canBeAdded(card, this);
+		if(valid == true){
+			cards.add(card);
+			return true;
+		}
+		return false;
+	}
+}
