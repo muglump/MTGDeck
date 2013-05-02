3c6581d797d90f531724ccf137b80b749f966312
muglump <korahel@gmail.com>
2013-05-02 12:46:30 -0400

    Tests for Basic Rules set, and stats done

--- src/deck/BasicRuleSetTest.java
+++ src/deck/BasicRuleSetTest.java
@@ -17,5 +17,63 @@ public class BasicRuleSetTest {
 		BasicRuleSet test = new BasicRuleSet();
 		Assert.assertEquals(60, test.DeckSize);
 	}
+	
+	@Test
+	public void testBasicValidDeck(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		assertEquals(true, test.DeckValid(testDeck));
+	}
+	
+	@Test
+	public void testBasicInvalidDeck(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		MTGCard testcard = new MTGCard("Test", "0", "0", "0", "0", "0", "0");
+		for(int i = 0; i <= 60; i++){
+			testDeck.cards.add(testcard);
+		}
+		assertEquals(false, test.DeckValid(testDeck));
+	}
+	
+	@Test
+	public void testBasicToString(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		assertEquals("BASIC", test.toString());
+	}
+	
+	@Test
+	public void testCanBeAddedNoMoreThen4(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		MTGCard testcard = new MTGCard("Test", "0", "0", "0", "0", "0", "0");
+		for(int i = 0; i < 59; i++){
+			testDeck.cards.add(testcard);
+		}
+		assertEquals(false, test.canBeAdded(testcard, testDeck));
+	}
+	
+	@Test
+	public void testCanBeAddedNoMoreThenDeckLimit(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		MTGCard testcard = new MTGCard("Test", "0", "0", "0", "0", "0", "0");
+		for(int i = 0; i < 59; i++){
+			testDeck.cards.add(testcard);
+		}
+		MTGCard testcard2 = new MTGCard("Test2", "0", "0", "0", "0", "0", "0");
+		assertEquals(true, test.canBeAdded(testcard2, testDeck));
+	}
+	
+	@Test
+	public void testCanBeAddedAnyNumberOfLands(){
+		BasicRuleSet test = new BasicRuleSet();
+		Deck testDeck = new Deck();
+		MTGCard testcard = new MTGCard("Test", "0", "Land", "0", "0", "0", "0");
+		for(int i = 0; i < 60; i++){
+			assertEquals(true, test.canBeAdded(testcard, testDeck));
+		}
+	}
 
 }
